8201e8fafcec87b6a3b5b83a30c04fb30492b013
Benjamin J Birch <thebenjaminjbirch@gmail.com>
2026-07-26 08:27:55 +0100

    Add files via upload

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,25 +1,3 @@
-const ageGate = document.getElementById("ageGate");
-const enterButton = document.getElementById("enterSite");
-const ageKey = "bjb-age-confirmed";
-
-const dismissAgeGate = () => {
-  if (!ageGate) return;
-  ageGate.style.setProperty("display", "none", "important");
-  ageGate.classList.add("hidden");
-  ageGate.hidden = true;
-  ageGate.setAttribute("aria-hidden", "true");
-  ageGate.remove();
-};
-
-try {
-  if (localStorage.getItem(ageKey) === "yes") dismissAgeGate();
-} catch {}
-
-enterButton?.addEventListener("click", () => {
-  try { localStorage.setItem(ageKey, "yes"); } catch {}
-  dismissAgeGate();
-});
-
 const menuButton = document.querySelector(".menu-button");
 const nav = document.querySelector(".site-nav");
 menuButton?.addEventListener("click", () => {
